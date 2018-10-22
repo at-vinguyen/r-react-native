58be2107dd358b7321801bd877aaa4687f2bb93a
at-vinguyen <vi.nguyen@asiantech.vn>
2018-10-22 10:56:28 +0700

    adding clear script to clean-up cache

--- MobileApp/src/app/App.js
+++ MobileApp/src/app/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput, Image} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput, Image, StatusBar} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -26,19 +26,24 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.bg}>
+        <StatusBar barStyle="light-content" />
+        {/* <View style={styles.bg}>
           <Image source={require('../assets/image/bg.jpg')} style={styles.backgroundImage} />
+        </View> */}
+        <View style={styles.topBar}>
+          <Text style={styles.appTitle}>Ms Home</Text>
         </View>
         <View style={styles.main}>
-          <Text style={styles.welcome}>Welcome to React Native!</Text>
-          <Text style={styles.instructions}>To get started, edit App.js</Text>
+          <Text style={styles.welcome}>Hi Guest!</Text>
+          <Text style={styles.description}>Don't loss your money!</Text>
+        </View>
+        <View style={styles.devMode}>
           <Text style={styles.instructions}>{instructions}</Text>
         </View>      
       </View>
     );
   }
 }
-const bg = '../assets/image/bg.jpg';
 const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
@@ -49,23 +54,48 @@ const styles = StyleSheet.create({
     height: '100%',
     width: '100%',
   },
+  topBar: {
+    position: 'relative',
+    height: 50,
+    width: '100%',
+    backgroundColor: '#8aba56',
+    justifyContent: 'flex-end',
+    paddingBottom: 4
+  },
+  appTitle: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 16
+  },
   container: {
     height: '100%',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#ffffff'
   },
   main: {
+    textAlign: 'center',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
   },
   welcome: {
     fontSize: 32,
-    textAlign: 'center',
-    margin: 10,
+    margin: 10
+  },
+  description: {
+    color: '#333333'
+  },
+  devMode: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    paddingTop: 10,
+    paddingBottom: 10
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: '#ffffff',
     marginBottom: 5,
   }
 });
